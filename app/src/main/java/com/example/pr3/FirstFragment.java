aab85d7fb7a2a59987bd0e280155b10c84238df9
Арина <rnltmst@mail.ru>
2023-03-08 17:20:32 +0300

    Добавлены Toast'ы и Log'и

--- app/src/main/java/com/example/pr3/FirstFragment.java
+++ app/src/main/java/com/example/pr3/FirstFragment.java
@@ -2,16 +2,93 @@ package com.example.pr3;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class FirstFragment extends Fragment {
 
     public FirstFragment() {
         super(R.layout.fragment_first);
+    }
+
+    @Override
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        ToastShow("FragOne onCreateView");
+        Log.i("FragOne", "onCreateView");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        ToastShow("FragOne onViewCreated");
+        Log.i("FragOne", "onViewCreated");
+
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState){
+        super.onViewStateRestored(savedInstanceState);
+        ToastShow("FragOne onViewStateRestored");
+        Log.i("FragOne", "onViewStateRestored");
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        ToastShow("FragOne onStart");
+        Log.i("FragOne", "onStart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        ToastShow("FragOne onResume");
+        Log.i("FragOne", "onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        ToastShow("FragOne onPause");
+        Log.i("FragOne", "onPause");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        ToastShow("FragOne onStop");
+        Log.i("FragOne", "onStop");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+        ToastShow("FragOne onSaveInstanceState");
+        Log.i("FragOne", "onSaveInstanceState");
+    }
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        ToastShow("FragOne onDestroyView");
+        Log.i("FragOne", "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        ToastShow("FragOne onDestroy");
+        Log.i("FragOne", "onDestroy");
+    }
+
+    private void ToastShow(String text) {
+        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
     }
 
 
--- app/src/main/java/com/example/pr3/SecondFragment.java
+++ app/src/main/java/com/example/pr3/SecondFragment.java
@@ -2,31 +2,97 @@ package com.example.pr3;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SecondFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SecondFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SecondFragment() {
         super(R.layout.fragment_second);
     }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ToastShow("OnCreate");
 
+    }
+    @Override
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        ToastShow("FragTwo onCreateView");
+        Log.i("FragTwo", "onCreateView");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        ToastShow("FragTwo onViewCreated");
+        Log.i("FragTwo", "onViewCreated");
 
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState){
+        super.onViewStateRestored(savedInstanceState);
+        ToastShow("FragTwo onViewStateRestored");
+        Log.i("FragTwo", "onViewStateRestored");
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        ToastShow("FragTwo onStart");
+        Log.i("FragTwo", "onStart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        ToastShow("FragTwo onResume");
+        Log.i("FragTwo", "onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        ToastShow("FragTwo onPause");
+        Log.i("FragTwo", "onPause");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        ToastShow("FragTwo onStop");
+        Log.i("FragTwo", "onStop");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+        ToastShow("FragTwo onSaveInstanceState");
+        Log.i("FragTwo", "onSaveInstanceState");
+    }
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        ToastShow("FragTwo onDestroyView");
+        Log.i("FragTwo", "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        ToastShow("FragTwo onDestroy");
+        Log.i("FragTwo", "onDestroy");
+    }
+
+    private void ToastShow(String text) {
+        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+    }
 }
